5bcfa6bf9507713009cc5516ebd4f5b65d4bc317
Paissa <92800796+Faculty178@users.noreply.github.com>
2022-10-16 16:51:53 +0900

    Add files via upload

--- 20221016.py
+++ 20221016.py
@@ -4,7 +4,7 @@ a = input()
 print(ord(a))
 
 
-#3009
+# #3009
 
 lista = []
 listb = []
@@ -24,4 +24,67 @@ for i in range(3):
         y = listb[i]
 
 print(x, y)
+
+
+#2439
+
+n = int(input())
+
+for i in range(n):
+    star = "*"*(i+1)
+    print(star.rjust(n))
+
+
+#1110
+
+n= int(input())
+cnt = 0
+new_n = n
+
+while(True):
+    cnt += 1
+    calc = n//10 + n%10
+    n = calc%10 + (n%10*10)
+   
+    if(n == new_n):
+        break
+
+
+print(cnt)
+
+
+
+#10250
+
+n = int(input())
+
+for _ in range(n):
+    h, w, num = map(int, input().split())
+
+    height = num % h
+    rnum = num//h + 1
+
+    if height == 0:
+        height = h
+        rnum -= 1
+
+    ans = height*100 + rnum
+    print(ans)
+
+
+#10757
+import sys
+a, b = map(int, sys.stdin.readline().split())
+print(a + b)
+
+
+#2869
+import sys
+up, down, v = map(int, sys.stdin.readline().split())
+
+if(v - down) % (up - down) == 0:
+    print((v-down) // (up-down))
+else:
+    print((v-down) // (up-down) +1)
+
 
